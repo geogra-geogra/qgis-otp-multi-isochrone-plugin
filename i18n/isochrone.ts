33e84b84b819ee0f52199b33a7e8eaaa7819fce7
Shota Yamamoto <yamamoto-shota1@ed.tmu.ac.jp>
2025-03-01 13:55:32 +0900

    translation

--- i18n/isochrone.ts
+++ i18n/isochrone.ts
@@ -9,132 +9,132 @@
         <translation>到達圏作成ツール</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="113"/>
+        <location filename="../isochrone.ui" line="201"/>
         <source>Single-Time</source>
         <translation>単一時間解析</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="123"/>
+        <location filename="../isochrone.ui" line="211"/>
         <source>Analysis Time</source>
         <translation>計測時刻</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="172"/>
+        <location filename="../isochrone.ui" line="260"/>
         <source>Multi-Time</source>
         <translation>複数時間解析</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="185"/>
+        <location filename="../isochrone.ui" line="273"/>
         <source>Start Time</source>
         <translation>開始時刻</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="222"/>
+        <location filename="../isochrone.ui" line="310"/>
         <source>End Time</source>
         <translation>終了時刻</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="263"/>
+        <location filename="../isochrone.ui" line="351"/>
         <source>Output Interval between Base Times (minutes)</source>
         <translation>基準時刻間での出力間隔(分)</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="281"/>
+        <location filename="../isochrone.ui" line="369"/>
         <source>Generate Statistical Mesh</source>
         <translation>統計メッシュを作成する</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="291"/>
+        <location filename="../isochrone.ui" line="379"/>
         <source>Generate Raster Mesh</source>
         <translation>ラスタメッシュを作成する</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="307"/>
+        <location filename="../isochrone.ui" line="395"/>
         <source>Raster Mesh Size (EPSG:4326 WGS84)</source>
         <translation>ラスタメッシュの大きさ(EPSG:4326 WGS84)</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="333"/>
+        <location filename="../isochrone.ui" line="421"/>
         <source>Use Existing Mesh</source>
         <translation>既存のメッシュを使う</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="380"/>
+        <location filename="../isochrone.ui" line="545"/>
         <source>Set Base</source>
         <translation>基準の設定</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="389"/>
+        <location filename="../isochrone.ui" line="584"/>
         <source>Specify Departure Time and Location</source>
         <translation>出発時刻と出発地点を指定</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="402"/>
+        <location filename="../isochrone.ui" line="597"/>
         <source>Specify Arrival Time and Location</source>
         <translation>到着時刻と到着地点を指定</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="446"/>
+        <location filename="../isochrone.ui" line="497"/>
         <source>Set Location</source>
         <translation>地点の設定</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="461"/>
+        <location filename="../isochrone.ui" line="110"/>
         <source>Specify Single Location</source>
         <translation>地点を指定</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="474"/>
+        <location filename="../isochrone.ui" line="123"/>
         <source>43.064344,141.361915</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="481"/>
+        <location filename="../isochrone.ui" line="130"/>
         <source>Get Coordinates</source>
         <translation>クリックした場所の位置座標を取得する</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="488"/>
+        <location filename="../isochrone.ui" line="137"/>
         <source>Set as Default Value for Next Time</source>
         <translation>この値を次回以降の既定値にする</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="504"/>
+        <location filename="../isochrone.ui" line="153"/>
         <source>Specify Multiple Locations</source>
         <translation>複数地点を指定</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="536"/>
+        <location filename="../isochrone.ui" line="628"/>
         <source>Server URL</source>
         <translation>サーバーURL</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="545"/>
+        <location filename="../isochrone.ui" line="471"/>
         <source>http://localhost:8080</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="577"/>
+        <location filename="../isochrone.ui" line="642"/>
         <source>Output Destination</source>
         <translation>出力先</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="621"/>
+        <location filename="../isochrone.ui" line="559"/>
         <source>Isochrone Shapes</source>
         <translation>到達圏の図形</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="630"/>
+        <location filename="../isochrone.ui" line="506"/>
         <source>Time Interval for Output Shapes at Each Time (minutes)</source>
         <translation>それぞれの時刻で出力する図形の時間の間隔(分)</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="646"/>
+        <location filename="../isochrone.ui" line="670"/>
         <source>Maximum Time for Output Shapes at Each Time (minutes)</source>
         <translation>それぞれの時刻で出力する図形の最大時間(分)</translation>
     </message>
     <message>
-        <location filename="../isochrone.ui" line="689"/>
+        <location filename="../isochrone.ui" line="766"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
@@ -183,6 +183,46 @@ p, li { white-space: pre-wrap; }
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;国勢調査の標準地域メッシュなどを想定しています。動作については、(株)MIERUNE提供のJapanese Grid Meshプラグインで作成したメッシュで行っています。グリッドは到達圏のジオメトリ全体を覆うサイズとしてください。(現時点ではClipに非対応)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
 </translation>
     </message>
+    <message>
+        <location filename="../isochrone.ui" line="734"/>
+        <source>&amp;walkSpeed=4&amp;bikeSwitchTime=5</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../isochrone.ui" line="85"/>
+        <source>MODE</source>
+        <translation>モード</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.ui" line="480"/>
+        <source>Max WalkDistance(m)</source>
+        <translation>最大歩行距離(m)</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.ui" line="649"/>
+        <source>add parameters</source>
+        <translation>追加パラメータ</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.ui" line="695"/>
+        <source>WALK</source>
+        <translation>徒歩</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.ui" line="705"/>
+        <source>TRANSIT</source>
+        <translation>交通機関</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.ui" line="715"/>
+        <source>CAR</source>
+        <translation>自動車</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.ui" line="722"/>
+        <source>BICYCLE</source>
+        <translation>自転車</translation>
+    </message>
 </context>
 <context>
     <name>Isochrone</name>
@@ -214,13 +254,13 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../isochrone.py" line="193"/>
         <source>No valid existing mesh layer selected.</source>
-        <translation>有効な既存のメッシュレイヤーが選択されていません。</translation>
+        <translation type="obsolete">有効な既存のメッシュレイヤーが選択されていません。</translation>
     </message>
 </context>
 <context>
     <name>IsochroneDialog</name>
     <message>
-        <location filename="../isochrone.py" line="263"/>
+        <location filename="../isochrone.py" line="252"/>
         <source>TabSingle has been selected.</source>
         <translation>TabSingleが選択されました。</translation>
     </message>
@@ -230,7 +270,7 @@ p, li { white-space: pre-wrap; }
         <translation type="obsolete">TabMultiが選択されました</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="265"/>
+        <location filename="../isochrone.py" line="254"/>
         <source>TabMulti has been selected</source>
         <translation>TabMultiが選択されました</translation>
     </message>
@@ -238,58 +278,68 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>IsochroneRequestHandler</name>
     <message>
-        <location filename="../isochrone.py" line="503"/>
+        <location filename="../isochrone.py" line="516"/>
         <source>Output directory is not specified. Process will be aborted.</source>
         <translation>保存先ディレクトリが指定されていません。処理を中断します。</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="513"/>
+        <location filename="../isochrone.py" line="526"/>
         <source>Cannot write to the specified directory: {base_path}. Process will be aborted.</source>
         <translation>指定されたディレクトリに書き込むことができません: {base_path}。処理を中断します。</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="528"/>
+        <location filename="../isochrone.py" line="541"/>
         <source>Failed to create directory: {str(e)}. Process will be aborted.</source>
         <translation>ディレクトリの作成に失敗しました: {str(e)}。処理を中断します。</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="462"/>
+        <location filename="../isochrone.py" line="475"/>
         <source>Empty response received</source>
         <translation>Empty response received</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="467"/>
+        <location filename="../isochrone.py" line="480"/>
         <source>Unsupported or missing content type</source>
         <translation>Unsupported or missing content type</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="472"/>
+        <location filename="../isochrone.py" line="485"/>
         <source>Network error occurred: {reply.errorString()}</source>
         <translation>Network error occurred: {reply.errorString()}</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.py" line="384"/>
+        <source>Please select more one layer.</source>
+        <translation>少なくとも1つの移動モードを選択してください。</translation>
     </message>
 </context>
 <context>
     <name>MeshHandler</name>
     <message>
-        <location filename="../isochrone.py" line="697"/>
+        <location filename="../isochrone.py" line="710"/>
         <source>Grid has been created. File path: {grid_file_name}</source>
         <translation>グリッドが作成されました。ファイルパス: {grid_file_name}</translation>
+    </message>
+    <message>
+        <location filename="../isochrone.py" line="716"/>
+        <source>No valid existing mesh layer selected.</source>
+        <translation>有効な既存のメッシュレイヤーが選択されていません。</translation>
     </message>
 </context>
 <context>
     <name>QgisLayerHandler</name>
     <message>
-        <location filename="../isochrone.py" line="963"/>
+        <location filename="../isochrone.py" line="991"/>
         <source>Failed to load layer</source>
         <translation>Failed to load layer</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="983"/>
+        <location filename="../isochrone.py" line="1011"/>
         <source>Failed to load raster layer</source>
         <translation>Failed to load raster layer</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="1008"/>
+        <location filename="../isochrone.py" line="1036"/>
         <source>Invalid raster layer.</source>
         <translation>Invalid raster layer.</translation>
     </message>
@@ -297,32 +347,32 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>Rasterizer</name>
     <message>
-        <location filename="../isochrone.py" line="725"/>
+        <location filename="../isochrone.py" line="749"/>
         <source>No valid mesh layer has been selected.</source>
         <translation>有効なメッシュレイヤーが選択されていません。</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="780"/>
+        <location filename="../isochrone.py" line="808"/>
         <source>Failed to load GeoJSON file: {geojson_file}</source>
         <translation>GeoJSONファイルの読み込みに失敗しました: {geojson_file}</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="802"/>
+        <location filename="../isochrone.py" line="830"/>
         <source>Rasterization completed: {output_raster_path}</source>
         <translation>ラスタ化が完了しました: {output_raster_path}</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="836"/>
+        <location filename="../isochrone.py" line="864"/>
         <source>{stat_type} raster has been created: {stat_file_path}</source>
         <translation>{stat_type}ラスタが作成されました: {stat_file_path}</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="867"/>
+        <location filename="../isochrone.py" line="895"/>
         <source>The mesh layer is invalid.</source>
         <translation>メッシュレイヤーが無効です。</translation>
     </message>
     <message>
-        <location filename="../isochrone.py" line="875"/>
+        <location filename="../isochrone.py" line="903"/>
         <source>The mesh layer contains no features.</source>
         <translation>メッシュレイヤーにフィーチャがありません。</translation>
     </message>
